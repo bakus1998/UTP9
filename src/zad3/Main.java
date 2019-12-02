850b8d8c93d275059576c8329d1d7bee7c2ca6b7
Krzysztof Baka <krzysztofbakapl@gmail.com>
2019-12-02 19:02:52 +0100

    Updated

--- src/zad3/Main.java
+++ src/zad3/Main.java
@@ -6,11 +6,9 @@
 
 package zad3;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -36,19 +34,19 @@ public class Main {
         }
       }));
 
-
       Map<String, List<String>> toShow =
               anagrams.entrySet()
                       .stream()
-                      .sorted(Map.Entry.comparingByKey())
+                      .sorted(myComparator())
                       .filter(s1 ->
                               s1.getValue().size() == maxSize)
                       .collect(
                               Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                                       (s1,s2)-> s1, LinkedHashMap::new));
 
-
-
+      for(Map.Entry<String, List<String>> entry : toShow.entrySet()){
+        Collections.sort(entry.getValue());
+      }
 
       toShow.forEach((e1, e2) -> {
         System.out.print( e1 + " ");
@@ -57,12 +55,24 @@ public class Main {
         }
         System.out.println("");
       });
-
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
 
-
+  public static Comparator<Map.Entry<String, List<String>>>  myComparator(){
+    Comparator<Map.Entry<String, List<String>>> comparator = new Comparator<Map.Entry<String, List<String>>>() {
+      @Override
+      public int compare(Map.Entry<String, List<String>> o1, Map.Entry<String, List<String>> o2) {
+       int i = o1.getKey().compareTo(o2.getKey());
+       if(i==0) {
+         return Integer.compare(o2.getValue().size(), o2.getValue().size());
+       }else{
+         return i;
+       }
+      }
+    };
+    return comparator;
+  }
 }
